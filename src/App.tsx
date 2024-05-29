0dec1982a1ec9a1ccde34c065980e74c12038ec1
Kostya Cholak <kostya.cholak@gmail.com>
2024-05-29 14:07:53 +0200

    fix: change carousel to horizontal

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[425px] w-[250px] rounded-xl" />
+      <Skeleton className="h-[425px] w-[350px] rounded-xl" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
@@ -43,8 +43,8 @@ export default function App() {
         </header>
         <main className="flex-1 gap-4 overflow-auto p-4">
           <div className="relative flex-col items-start gap-8 md:flex" x-chunk="dashboard-03-chunk-0">
-            <Carousel orientation="vertical">
-              <CarouselContent className="max-h-[500px]">
+            <Carousel orientation="horizontal">
+              <CarouselContent className="max-h-[500px] max-w-[400px]">
                 <CarouselItem>
                   <SkeletonCard />
                 </CarouselItem>
