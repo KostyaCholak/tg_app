277810c72baa95163b129854583189f62fbc7dbe
Kostya Cholak <kostya.cholak@gmail.com>
2024-05-29 14:00:57 +0200

    fix: change build page url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tg_app/',
   plugins: [react(), basicSsl()],
   build: {
     outDir: './docs'
